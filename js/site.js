b2f10f74ace610bf57f68f7dc37a089c13bf10f6
Elleyse Na <nae122@newschool.edu>
2023-10-18 16:27:45 -0400

    added to js file

---
+++ js/site.js
@@ -0,0 +1,14 @@
+// should be at least 100 lines of code, have 10 properies at the 
+// top level, have 3 nested properties (object within object), at 
+// least two fuctions, add NUMBER, STRING, BOOLEAN, FUNCTION, OBJECT,
+//ARRAY, 
+
+const person = {
+    firstName: "ADHD",
+    lastName: "Happy",
+    lastName2: "Angry",
+    fullName :function() {
+        return this.firstName + " " + this.lastName + this.lastName2;
+    }
+};
+
