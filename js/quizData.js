32d1ddbef02f40fd44aa9caee6f123a1f99ede53
Elleyse Na <nae122@newschool.edu>
2023-11-06 17:26:14 -0500

    deleted and modified files

--- js/quizData.js
+++ js/quizData.js
@@ -1,26 +1,3 @@
-// // app.js
-// function displayUser() {
-//     var userInfoContainer = document.getElementById('userInfo');
-//     var textEditorContent = document.getElementById('textEditor').value;
-
-//     userInfoContainer.innerHTML = '';
-
-//     for (var key in user) {
-//         if (Array.isArray(user[key])) {
-//             userInfoContainer.innerHTML += `<p>${key}: ${user[key].join(', ')}</p>`;
-//         } else if (typeof user[key] !== 'function') {
-//             userInfoContainer.innerHTML += `<p>${key}: ${user[key]}</p>`;
-//         }
-//     }
-
-//     user.displayInfo();
-
-//     userInfoContainer.innerHTML += `<p>Text Editor Content: ${textEditorContent}</p>`;
-
-//     saveToLocalStorage('textEditorContent', textEditorContent);
-
-
-// }
 function saveToLoacalStorage(key, value){
     localStorage.setItem(key, value);
 
@@ -39,5 +16,4 @@ function loadSavedContent() {
     }
 }
 
-// Call the function to load saved content when the page loads
 loadSavedContent();
